d3641afcb019a68cbb12dfbab33da7bbe6762e0f
gabrielbog <bogoslovgabriel@gmail.com>
2024-03-25 19:55:37 +0200

    Created a debug class for the console operations

---
+++ AnimalSounds/Classes/Debug.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSounds
+{
+    public class Debug
+    {
+        public static List<Animal> getAnimalList()
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Canine());
+            animals.Add(new Feline());
+            animals.Add(new Feline());
+            animals.Add(new Bird());
+            animals.Add(new Bird());
+
+            return animals;
+        }
+
+        public static void printAnimalSounds(List<Animal> animals)
+        {
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.GetType() + " makes sound: " + animal.GetSound());
+            }
+        }
+    }
+}
--- AnimalSounds/Program.cs
+++ AnimalSounds/Program.cs
@@ -10,18 +10,8 @@ namespace AnimalSounds
     {
         static void Main(string[] args)
         {
-            List<Animal> animals = new List<Animal>();
-            animals.Add(new Canine());
-            animals.Add(new Feline());
-            animals.Add(new Feline());
-            animals.Add(new Bird());
-            animals.Add(new Bird());
-
-            foreach(Animal animal in animals)
-            {
-                Console.WriteLine(animal.GetType() + " makes sound: " + animal.GetSound());
-            }
-
+            List<Animal> animals = Debug.getAnimalList();
+            Debug.printAnimalSounds(animals);
             Console.ReadKey();
         }
     }
